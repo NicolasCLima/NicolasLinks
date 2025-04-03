34061f0bf990478ae66d0814f799bb719a050fd9
NicolasCLima <nicolasclima00@gmail.com>
2025-04-02 22:12:38 -0300

    segundo botão

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { GiBiceps } from "react-icons/gi";
 import { FaGithub } from "react-icons/fa";
+import Linkedin from './components/Linkedin';
+
+
 function App() {
   
 
   return (
     <div className='container-app'>
       <h1>MEUS LINKS</h1>
-      <a href="https://github.com/NicolasCLima" className='links'target='_blank'><FaGithub /> GITHUB</a>
-
+      <a href="https://github.com/NicolasCLima" className='links'target='_blank'><FaGithub /> GITHUB <FaGithub /></a>
+      <Linkedin />
     </div>
+
   )
 }
 
---
+++ src/components/Linkedin.jsx
@@ -0,0 +1,11 @@
+import { FaLinkedin } from "react-icons/fa";
+
+function Linkedin() {
+  return (
+    <div className='container-linkedin links' >
+      <FaLinkedin /> Linkedin <FaLinkedin />
+    </div>
+  )
+}
+
+export default Linkedin
