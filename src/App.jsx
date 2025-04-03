9b951365b9245eb8bad47c47728cd8ae717d5d06
NicolasCLima <nicolasclima00@gmail.com>
2025-04-02 21:15:50 -0300

    Lançado o MVP do projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { GiBiceps } from "react-icons/gi";
+import { FaGithub } from "react-icons/fa";
+function App() {
+  
+
+  return (
+    <div className='container-app'>
+      <h1>MEUS LINKS</h1>
+      <a href="https://github.com/NicolasCLima" className='links'target='_blank'><FaGithub /> GITHUB</a>
+
+    </div>
+  )
+}
+
+export default App
